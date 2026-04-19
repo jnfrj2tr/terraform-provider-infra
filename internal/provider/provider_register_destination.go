62db9171da3f3c5620c1a52f6374071fbd01e950
J. Farraj <jnfrj2tr@users.noreply.github.com>
2026-04-20 02:54:44 +0800

    Add TODO note about future non-Kubernetes destination type support

--- internal/provider/provider_register_destination.go
+++ internal/provider/provider_register_destination.go
@@ -4,6 +4,7 @@ func init() {
 	// Register the destination resource so Terraform can manage Infra destinations.
 	// Also register as a data source to allow read-only lookups of existing destinations.
 	// Note: destinations represent connectors (e.g. Kubernetes clusters) managed by Infra.
+	// TODO: explore adding support for non-Kubernetes destination types in the future.
 	resources["infra_destination"] = resourceDestination()
 	datasources["infra_destination"] = datasourceDestination()
 }
